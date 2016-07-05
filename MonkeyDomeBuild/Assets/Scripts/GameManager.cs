4e29075b2f4e27eeb7df7f07f5d4f67792dcb762
Your Name <chooker101@gmail.com>
2016-07-05 11:59:27 -0700

    Added code structure and classes

---
+++ MonkeyDomeBuild/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+	private static GameManager s_Instance;
+	public static GameManager Instance
+	{
+		get
+		{
+			if (s_Instance == null)
+			{
+				s_Instance = FindObjectOfType<GameManager>();
+			}
+			return s_Instance;
+		}
+	}
+
+	public Player gmplayer;
+	//public ButtonManager gmbuttonmanager;
+}
