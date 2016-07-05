94e1cccb12fe37cc6dd1882ee99616403aed02f7
EtonL <eton83816@hotmail.com>
2016-07-05 10:09:14 -0700

    player movement

---
+++ MonkeyDomeBuild/Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAction : MonoBehaviour {
+
+    public float moveForce;
+    public float speedLimit;
+    private Rigidbody m_rigid;
+    public Vector3 mov;
+
+	void Start ()
+    {
+        moveForce = 20f;
+        speedLimit = 8f;
+        m_rigid = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+    {
+        bool mLeft = Input.GetKey(KeyCode.A);
+        bool mRight = Input.GetKey(KeyCode.D);
+        Vector3 movement = new Vector3();
+        if (mLeft && Mathf.Abs(m_rigid.velocity.x) < speedLimit)
+        {
+            movement.x = -moveForce;
+        }
+        else if (mRight && Mathf.Abs(m_rigid.velocity.x) < speedLimit)
+        {
+            movement.x = moveForce;
+        }
+        m_rigid.AddForce(movement);
+        mov = m_rigid.velocity;
+	}
+}
