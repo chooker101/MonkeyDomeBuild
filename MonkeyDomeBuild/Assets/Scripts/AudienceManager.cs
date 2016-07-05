475a31846a792f3e4e0d77a32df83d5c4b689a42
marcofilippobruno <mfilippobruno@gmail.com>
2016-07-05 15:40:58 -0700

    material changes and AudienceManager script setup

---
+++ MonkeyDomeBuild/Assets/Scripts/AudienceManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/*
+ * This script will:
+ * - track audience approval rating of players
+ *      - which involves paying attention for player behaviors (cheap throws, taunting, risky plays)
+ * - manage audience blurbs to correspond with player ratings
+ *      - including good/bad feedback and call for certain playstyles to specific players
+ * - store information for trophy rewards at end of match
+ */
+
+public class AudienceManager : MonoBehaviour {
+
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
