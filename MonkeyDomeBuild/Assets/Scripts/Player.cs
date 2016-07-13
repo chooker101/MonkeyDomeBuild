ddfedb7fb0f1160f975ae904a2d6631b1a1a5629
s83816 <eton83816@hotmail.com>
2016-07-12 17:01:41 -0700

    some minor fixes on Player and PlayerAction scripts

--- MonkeyDomeBuild/Assets/Scripts/Player.cs
+++ MonkeyDomeBuild/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : Actor
 {
@@ -40,10 +41,79 @@ public class Player : Actor
     public int stat_jump = 0;
     public int stat_throw = 0;
     public int stat_ballGrab = 0;
+
+    public List<float> monkeyStats;
+    public List<float> gorillaStats;
     void Start ()
 	{
-	
-	}
+        layerMask = 1 << LayerMask.NameToLayer("Floor");
+        moveForce = 100f;
+        jumpForce = 65f;
+        speedLimit = 12f;
+        throwForce = 40f;
+        downForce = 80f;
+        tempDownForce = downForce;
+        downForceIncrement = 100f; // per second
+        maxDownForce = 200f;
+        climbForce = 200f; ;
+        climbSpeedLimit = speedLimit;
+        m_rigid = GetComponent<Rigidbody>();
+        normalDrag = m_rigid.drag;
+        climbDrag = 12f;
+
+    }
+    protected void StoreMonkeyStats()
+    {
+        monkeyStats.Add(moveForce);
+        monkeyStats.Add(jumpForce);
+        monkeyStats.Add(speedLimit);
+        monkeyStats.Add(downForce);
+        monkeyStats.Add(downForceIncrement);
+        monkeyStats.Add(maxDownForce);
+        monkeyStats.Add(climbForce);
+        monkeyStats.Add(climbSpeedLimit);
+        monkeyStats.Add(normalDrag);
+        monkeyStats.Add(climbDrag);
+    }
+    protected void ApplyMonkeyStats()
+    {
+        moveForce = monkeyStats[0];
+        jumpForce = monkeyStats[1];
+        speedLimit = monkeyStats[2];
+        downForce = monkeyStats[3];
+        downForceIncrement = monkeyStats[4];
+        maxDownForce = monkeyStats[5];
+        climbForce = monkeyStats[6];
+        climbSpeedLimit = monkeyStats[7];
+        normalDrag = monkeyStats[8];
+        climbDrag = monkeyStats[9];
+    }
+    protected void SetGorillaStats()
+    {
+        gorillaStats.Add(moveForce);
+        gorillaStats.Add(jumpForce);
+        gorillaStats.Add(speedLimit);
+        gorillaStats.Add(downForce);
+        gorillaStats.Add(downForceIncrement);
+        gorillaStats.Add(maxDownForce);
+        gorillaStats.Add(climbForce);
+        gorillaStats.Add(climbSpeedLimit);
+        gorillaStats.Add(normalDrag);
+        gorillaStats.Add(climbDrag);
+    }
+    protected void ApplyGorillaStats()
+    {
+        moveForce = gorillaStats[0];
+        jumpForce = gorillaStats[1];
+        speedLimit = gorillaStats[2];
+        downForce = gorillaStats[3];
+        downForceIncrement = gorillaStats[4];
+        maxDownForce = gorillaStats[5];
+        climbForce = gorillaStats[6];
+        climbSpeedLimit = gorillaStats[7];
+        normalDrag = gorillaStats[8];
+        climbDrag = gorillaStats[9];
+    }
     protected void CheckInputs()
     {
         switch (whichPlayer)
@@ -73,6 +143,7 @@ public class Player : Actor
                     mY = -1;
                 }
                 break;
+
             case 2:
                 mX = Input.GetAxis("p2_joy_x");
                 mY = -Input.GetAxis("p2_joy_y");
@@ -80,6 +151,7 @@ public class Player : Actor
                 mCatch = Input.GetButtonDown("p2_catch/throw");
                 //mClimb = Input.GetButtonDown("p2_climb");
                 break;
+
             case 3:
                 mX = Input.GetAxis("p3_joy_x");
                 mY = -Input.GetAxis("p3_joy_y");
@@ -254,9 +326,53 @@ public class Player : Actor
     {
         return haveBall;
     }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            if (!ballInRange && !other.transform.parent.GetComponent<Rigidbody>().isKinematic)
+            {
+                ballInRange = true;
+                ball = other.gameObject;
+            }
+        }
+        if (other.gameObject.CompareTag("Vine") && !canClimb)
+        {
+            canClimb = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            ballInRange = false;
+            haveBall = false;
+            ball = null;
+            ballHolding = null;
+        }
+        if (other.gameObject.CompareTag("Vine") && canClimb)
+        {
+            canClimb = false;
+            isClimbing = false;
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        OnTriggerEnter(other);
+    }
     protected void StompCheck()
     {
 
+    }
+    protected void MonkeyToGorilla()
+    {
+        isGorilla = true;
+        transform.localScale += new Vector3(1f, 1f, 0f);
+    }
+    protected void GorillaToMonkey()
+    {
+        isGorilla = false;
+        transform.localScale += new Vector3(-1f, -1f, 0f);
     }
 
 }
--- MonkeyDomeBuild/Assets/Scripts/PlayerAction.cs
+++ MonkeyDomeBuild/Assets/Scripts/PlayerAction.cs
@@ -4,34 +4,6 @@ using System.Collections.Generic;
 
 public class PlayerAction : Player
 {
-
-
-
-
-    
-    // Player Stats
-
-
-    void Start()
-    {
-        //whichPlayer = 0;
-        moveForce = 100f;
-        jumpForce = 65f;
-        speedLimit = 12f;
-        throwForce = 40f;
-        downForce = 80f;
-        tempDownForce = downForce;
-        downForceIncrement = 100f; // per second
-        maxDownForce = 200f;
-        climbForce = 200f; ;
-        climbSpeedLimit = speedLimit;
-        m_rigid = GetComponent<Rigidbody>();
-        normalDrag = m_rigid.drag;
-        climbDrag = 12f;
-        layerMask = 1 << LayerMask.NameToLayer("Floor");
-        
-    }
-
     void FixedUpdate()
     {
         CheckInputs();
@@ -46,51 +18,26 @@ public class PlayerAction : Player
             }
             else
             {
-                StompCheck();
                 CatchCheck();
             }
         }
         else
         {
-
-            CatchCheck();
+            StompCheck();
         }
-
         mov = m_rigid.velocity;
-    }
-    void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Ball"))
+        if (Input.GetKeyDown(KeyCode.G))
         {
-            if (!ballInRange && !other.transform.parent.GetComponent<Rigidbody>().isKinematic)
+            if (isGorilla)
             {
-                ballInRange = true;
-                ball = other.gameObject;
+                GorillaToMonkey();
+            }
+            else
+            {
+                MonkeyToGorilla();
             }
         }
-        if (other.gameObject.CompareTag("Vine") && !canClimb)
-        {
-            canClimb = true;
-        }
     }
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.CompareTag("Ball"))
-        {
-            ballInRange = false;
-            haveBall = false;
-            ball = null;
-            ballHolding = null;
-        }
-        if (other.gameObject.CompareTag("Vine") && canClimb)
-        {
-            canClimb = false;
-            isClimbing = false;
-        }
-    }
-    void OnTriggerStay(Collider other)
-    {
-        OnTriggerEnter(other);
-    }
+
 
 }
