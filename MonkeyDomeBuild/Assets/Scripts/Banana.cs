09c6bafab2e0a9ddefdf1fe7cf30579e64f6e749
s83816 <eton83816@hotmail.com>
2016-07-19 12:51:28 -0700

    add turret test

---
+++ MonkeyDomeBuild/Assets/Scripts/Banana.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Banana : MonoBehaviour {
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Vine"))
+        {
+            //Debug.Log(transform.position.x);
+        }
+    }
+}
