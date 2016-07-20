baae875f231ba4f60d007bcf8017aae1bdec5362
s83816 <eton83816@hotmail.com>
2016-07-19 17:36:31 -0700

    turret update

--- MonkeyDomeBuild/Assets/Scripts/Turret.cs
+++ MonkeyDomeBuild/Assets/Scripts/Turret.cs
@@ -3,61 +3,58 @@ using System.Collections;
 
 public class Turret : MonoBehaviour {
 
-    public Transform Target;
+    public GameObject stuffToShoot;
+    private Rigidbody projectile;
+    private Transform myTransform;
+    private Vector3 velocity;
+    public Transform target;
     public float firingAngle = 45.0f;
     public float gravity = 9.8f;
-
-    public GameObject stuffToShoot;
-    private Transform projectile;
-    private Transform myTransform;
 
     void Awake()
     {
         myTransform = transform;
-        GameObject go = (GameObject)Instantiate(stuffToShoot, transform.position, transform.rotation);
-        projectile = go.transform;
     }
-
     void Start()
     {
 
-        StartCoroutine(SimulateProjectile());
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Fire();
+        }
     }
 
-
-    IEnumerator SimulateProjectile()
+    public void Fire()
     {
-        // Short delay added before Projectile is thrown
-        yield return new WaitForSeconds(1.5f);
-
-        // Move projectile to the position of throwing object + add some offset if needed.
-        projectile.position = myTransform.position + new Vector3(0, 0.0f, 0);
-
-        // Calculate distance to target
-        float target_Distance = Vector3.Distance(projectile.position, Target.position);
-
-        // Calculate the velocity needed to throw the object to the target at specified angle.
+        // work perfect if no drag
+        // kinda work with very little drag like around 0.05
+        GameObject go = (GameObject)Instantiate(stuffToShoot, transform.position, transform.rotation);
+        projectile = go.GetComponent<Rigidbody>();
+        float target_Distance = Vector3.Distance(projectile.position, target.position);
+        firingAngle = Random.Range(30, 60);
         float projectile_Velocity = target_Distance / (Mathf.Sin(2 * firingAngle * Mathf.Deg2Rad) / gravity);
-
-        // Extract the X  Y componenent of the velocity
-        float Vx = Mathf.Sqrt(projectile_Velocity) * Mathf.Cos(firingAngle * Mathf.Deg2Rad);
-        float Vy = Mathf.Sqrt(projectile_Velocity) * Mathf.Sin(firingAngle * Mathf.Deg2Rad);
-
-        // Calculate flight time.
-        float flightDuration = target_Distance / Vx;
-
-        // Rotate projectile to face the target.
-        projectile.rotation = Quaternion.LookRotation(Target.position - projectile.position);
-
-        float elapse_time = 0;
-
-        while (elapse_time < flightDuration)
+        float vx = Mathf.Sqrt(projectile_Velocity) * Mathf.Cos(firingAngle * Mathf.Deg2Rad);
+        float vy = Mathf.Sqrt(projectile_Velocity) * Mathf.Sin(firingAngle * Mathf.Deg2Rad);
+        if (transform.position.x > target.transform.position.x)
         {
-            projectile.Translate(0, (Vy - (gravity * elapse_time)) * Time.deltaTime, Vx * Time.deltaTime);
-
-            elapse_time += Time.deltaTime;
-
-            yield return null;
+            velocity = new Vector3(-vx, vy);
         }
+        else
+        {
+            velocity = new Vector3(vx, vy);
+        }
+        projectile.velocity = velocity;
+        Destroy(go, 4f);
+    }
+    public void SetTarget(GameObject tar)
+    {
+        target = tar.transform;
+    }
+    public void SetBullet(GameObject obj)
+    {
+        stuffToShoot = obj;
     }
 }
