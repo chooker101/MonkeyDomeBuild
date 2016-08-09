306d06f63ad37f708a039f927929bfc7ced43f85
RayFlower <beernaerts@live.ca>
2016-08-08 21:28:24 -0700

    Began creating pregame room

---
+++ MonkeyDomeBuild/Assets/PreGameTimer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PreGameTimer : MonoBehaviour
+{
+
+    Text text;
+    public float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else if(timer <= 0)
+        {
+            Application.LoadLevel("testingroom");
+        }
+        text.text = "Pre-game Room\n" + timer.ToString("F2");
+    }
+}
