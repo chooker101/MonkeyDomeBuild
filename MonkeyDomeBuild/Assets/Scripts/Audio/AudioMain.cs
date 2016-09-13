019ab71003bd7b4efd2904a27cc3b3c47bd74dc2
marcofilippobruno <mfilippobruno@gmail.com>
2016-09-13 09:21:26 -0700

    audio logic in main and pregameroom
    disable AudioHolder object if you want to stop hearing music

---
+++ MonkeyDomeBuild/Assets/Scripts/Audio/AudioMain.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent(typeof(AudioSource))]
+public class AudioMain : MonoBehaviour
+{
+
+    public AudioClip startTrack;
+    public AudioClip mainLoop;
+    private AudioSource levelAudio;
+
+    // Use this for initialization
+    void Start()
+    {
+        levelAudio = GetComponent<AudioSource>();
+        levelAudio.loop = true; ;
+
+        StartCoroutine(PlayStartTrack());
+    }
+
+    IEnumerator PlayStartTrack()
+    {
+        levelAudio.clip = startTrack;
+        levelAudio.Play();
+        yield return new WaitForSeconds(levelAudio.clip.length);
+        levelAudio.clip = mainLoop;
+        levelAudio.Play();
+
+    }
+}
