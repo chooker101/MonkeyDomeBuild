9183cfa8cde7a45281e8de1a925f2680e7ac7d07
Your Name <chooker101@gmail.com>
2016-09-13 12:04:25 -0700

    pause its hammertime

---
+++ MonkeyDomeBuild/Assets/Scripts/Managers/PauseManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public bool isGamePaused = false;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButton("space"))
+		{
+			isGamePaused = true;
+			Time.timeScale = 0;
+		}
+		while (isGamePaused)
+		{
+			if(Input.GetButton("space"))
+			{
+				isGamePaused = false;
+				Time.timeScale = 1;
+			}
+		}
+	}
+}
