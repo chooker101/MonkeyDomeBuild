7af1bfd2f63a46a0bb5b3e076e2c3a4f946eb64b
James Csupak <jamescsupak@gmail.com>
2016-09-13 11:56:00 -0700

    Updates to AudienceManager events

--- MonkeyDomeBuild/Assets/Scripts/Managers/AudienceManager.cs
+++ MonkeyDomeBuild/Assets/Scripts/Managers/AudienceManager.cs
@@ -14,17 +14,238 @@ using System.Collections.Generic;
 public class AudienceManager : MonoBehaviour
 {
 
+    public enum AudienceEvent
+    {
+        Jump, //=0
+        Targets,
+        Buzzer,
+        Bananas,
+        KnockDown,
+        Intercept,
+        BouncePass
+    }
+
+    public enum PlayerNum
+    {
+        Player1, //=0
+        Player2,
+        Player3,
+        Player4,
+        Player5
+    }
+
+    private float newEventTimer;
+    private float activeEventLifeTime;
+    [SerializeField]
+    private string activeEventTitle;
+    private bool eventActive = false;
+    private PlayerNum currentEvenWinner;
+    private PlayerNum currentEventLoser;
+
+    public static Dictionary<PlayerNum, int> audPlayerOpinion = new Dictionary<PlayerNum, int>();
+    public static Dictionary<PlayerNum, int> eventGoalCompleted = new Dictionary<PlayerNum, int>();
+
+    private AudienceEvent randEvent;
+    private AudienceEvent currentEvent;
+
+    private uint cachedTotalNumPlayers;
+
+    //specific event variables for event completion checks
+    private int startingTargetTier;
+    private int targetUpScore = 10; //TODO test score reward for targets upgraded and adjust
+
 
     void Start()
     {
-
+        //initialize the audience opinion levels to 0
+        for (int i = 0; i < audPlayerOpinion.Count; ++i)
+        {
+            audPlayerOpinion.Add((PlayerNum)i, 0);
+            eventGoalCompleted.Add((PlayerNum)i, 0);
+        }
+        EventEnd();
+        cachedTotalNumPlayers = GameManager.Instance.TotalNumberofPlayers;
     }
 
     void Update()
     {
-        UpdateAudienceMood();
+        if(newEventTimer <= 0 && !eventActive)
+        {
+            StartAudienceEvent();
+        }
+        if(!eventActive)
+        {
+            newEventTimer -= Time.fixedDeltaTime;
+        }
+        if (eventActive)
+        {
+            switch (currentEvent)
+            {
+
+                case AudienceEvent.Jump:
+                    JumpEvent();
+                    break;
+                case AudienceEvent.Targets:
+                    TargetUpgradeEvent();
+                    break;
+                case AudienceEvent.Buzzer:
+                    BuzzerBeaterEvent();
+                    break;
+                case AudienceEvent.Bananas:
+                    BananaCatchEvent();
+                    break;
+                case AudienceEvent.KnockDown:
+                    KnockDownMonkeysEvent();
+                    break;
+                case AudienceEvent.Intercept:
+                    GorillaInterceptEvent();
+                    break;
+                case AudienceEvent.BouncePass:
+                    BouncePassEvent();
+                    break;
+            }
+        }
     }
+
     void UpdateAudienceMood()
+    {
+
+    }
+
+    void StartAudienceEvent()
+    {
+        currentEvent = RandomAudienceEvent();
+
+        for (int i = 0; i < eventGoalCompleted.Count; ++i)
+        {
+            eventGoalCompleted[(PlayerNum)i] = 0;
+        }
+
+        switch (currentEvent)
+        {
+
+            case AudienceEvent.Jump:
+                activeEventTitle = "Jump Around!";
+                activeEventLifeTime = UnityEngine.Random.Range(5.0f, 8.0f);
+                break;
+            case AudienceEvent.Targets:
+                activeEventTitle = "Upgrade Targets!";
+                startingTargetTier = GameManager.Instance.gmTargetManager.targetTier;
+                activeEventLifeTime = UnityEngine.Random.Range(13.0f, 15.0f);
+                break;
+            case AudienceEvent.Buzzer:
+                activeEventTitle = "Buzzer Beater";
+                activeEventLifeTime = UnityEngine.Random.Range(10.0f, 13.0f);
+                break;
+            case AudienceEvent.Bananas:
+                activeEventTitle = "Catch Bananas!";
+                activeEventLifeTime = UnityEngine.Random.Range(5.0f, 8.0f);
+                break;
+            case AudienceEvent.KnockDown:
+                activeEventTitle = "Knock Down Monkeys!";
+                activeEventLifeTime = UnityEngine.Random.Range(13.0f, 16.0f);
+                break;
+            case AudienceEvent.Intercept:
+                activeEventTitle = "Gorilla Intercept!";
+                activeEventLifeTime = UnityEngine.Random.Range(8.0f, 10.0f);
+                break;
+            case AudienceEvent.BouncePass:
+                activeEventTitle = "One Bounce Pass!";
+                activeEventLifeTime = UnityEngine.Random.Range(3.0f, 5.0f);
+                break;
+        }
+        eventActive = true;
+        StartCoroutine(EventTimer());
+    }
+
+    AudienceEvent RandomAudienceEvent()
+    {
+        System.Array rand = System.Enum.GetValues(typeof(AudienceEvent));
+        randEvent = (AudienceEvent)rand.GetValue(UnityEngine.Random.Range(0, rand.Length));
+        return randEvent;
+    }
+
+    void EventEnd()
+    {
+        eventActive = false;
+        newEventTimer = UnityEngine.Random.Range(8.0f, 12.0f);
+    }
+
+    IEnumerator EventTimer()
+    {
+        //When event is called
+        yield return new WaitForSeconds(activeEventLifeTime);
+        //after timer finishes
+        eventActive = false;
+    }
+
+    void JumpEvent()
+    {
+        for(int i = 0; i < (int)cachedTotalNumPlayers; ++i)
+        {
+            if (GameManager.Instance.gmInputs[i].mJump)
+            {
+                ++eventGoalCompleted[(PlayerNum)i];
+            }
+        }
+
+        if (!eventActive)
+        {
+            for(int i = 0; i < (int)cachedTotalNumPlayers; ++i)
+            {
+                audPlayerOpinion[(PlayerNum)i] += (int)(0.5 * eventGoalCompleted[(PlayerNum)i]);
+            }
+            EventEnd();
+        }
+    }
+
+    void TargetUpgradeEvent()
+    {
+        bool targetsUpgraded = false;
+        if(GameManager.Instance.gmTargetManager.targetTier > startingTargetTier)
+        {
+            targetsUpgraded = true;
+        }
+
+        if (targetsUpgraded)
+        {
+            for(int i = 0; i < (int)cachedTotalNumPlayers; ++i)
+            {
+                if(GameManager.Instance.gmPlayers[i].GetComponent<Player>().characterType is Monkey)
+                {
+                    audPlayerOpinion[(PlayerNum)i] += targetUpScore;
+                }
+            }
+            eventActive = false;
+        }
+
+        if (!eventActive)
+        {
+            EventEnd();
+        }
+    }
+
+    void BuzzerBeaterEvent()
+    {
+        
+    }
+
+    void BananaCatchEvent()
+    {
+
+    }
+
+    void KnockDownMonkeysEvent()
+    {
+
+    }
+
+    void GorillaInterceptEvent()
+    {
+
+    }
+
+    void BouncePassEvent()
     {
 
     }
