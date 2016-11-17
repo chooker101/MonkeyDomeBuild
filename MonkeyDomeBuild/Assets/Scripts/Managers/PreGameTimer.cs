d894ad8cb1cd1a1a19da31e0400c5a9f3b002cdd
RayFlower <xionoblivion@gmail.com>
2016-11-17 12:38:25 -0800

    Pregame room stuff gucci

--- MonkeyDomeBuild/Assets/Scripts/Managers/PreGameTimer.cs
+++ MonkeyDomeBuild/Assets/Scripts/Managers/PreGameTimer.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public enum GameState
 {
@@ -11,7 +12,12 @@ public enum GameState
     Pregame_GorillaSet,
     Game_Start
 }
-
+public enum TutorialState
+{
+    Jump,
+    Catch,
+    Climb
+}
 public class PreGameTimer : MonoBehaviour
 {
     public Object gameScene;
@@ -32,10 +38,17 @@ public class PreGameTimer : MonoBehaviour
 
     GameObject newSpinner;
     public GameState gameState = GameState.Null;
-
-    private bool spinnerSpawned = false;
+    public TutorialState tutorialState = TutorialState.Jump;
     public bool gorillaSet = false;
     public bool gorillaSmashed = false;
+    private List<int> monkeysActions = new List<int>();
+    public bool allMonkeysJumped = false;
+    public bool allMonkeysCatchBall = false;
+    public bool allMonkeysClimbed = false;
+    public bool allMonkeysThrew = false;
+    public bool allMonkeysCalled = false;
+
+    private bool spinnerSpawned = false;
     private GameObject[] colourTargets;
     private bool loadedScene = false;
 
@@ -43,16 +56,21 @@ public class PreGameTimer : MonoBehaviour
     void Start()
     {
         gameState = GameState.Pregame_PickingColours;
-        sign_catch.SetActive(true);
+        sign_catch.SetActive(false);
         sign_jump.SetActive(true);
         sign_callForBall.SetActive(true);
-        sign_climbVines.SetActive(true);
+        sign_climbVines.SetActive(false);
         sign_throw.SetActive(true);
         sign_throwLong.SetActive(true);
 
         sign_gorillaSmash.SetActive(false);
         
         colourTargets = GameObject.FindGameObjectsWithTag("ColourTarget");
+        for(int i = 0; i < GameManager.Instance.TotalNumberofPlayers; i++)
+        {
+            //monkeysJumped.Add
+        }
+        StartCoroutine(TutorialSequence());
     }
 
     // Update is called once per frame
@@ -67,11 +85,22 @@ public class PreGameTimer : MonoBehaviour
             gorillaSmashed = true;
         }
 
+        // Check to see what signs come up
+        //if (!monkeysJumped)
+        //{
+            //for(int i = 0; i < GameManager.Instance.TotalNumberofPlayers; i++)
+            //{
+
+            //}
+        //}
+
+        // Check the game state to set
         if (!AllTargetsHit()) { gameState = GameState.Pregame_PickingColours; }
         else if(AllTargetsHit() && spinnerSpawned && !gorillaSet) { gameState = GameState.Pregame_SpinnerSpinning; }
         else if(AllTargetsHit() && spinnerSpawned && gorillaSet) { gameState = GameState.Pregame_GorillaSet; }
 
-        if(gameState == GameState.Pregame_PickingColours)
+        // Defines actions for the room based on the game state
+        /*if(gameState == GameState.Pregame_PickingColours)
         {
             if(!sign_catch.activeSelf)
             {
@@ -115,7 +144,7 @@ public class PreGameTimer : MonoBehaviour
             {
                 sign_gorillaSmash.SetActive(true);
             }
-        }
+        }*/
 
         // spawns a spinner that chooses a player to be a gorilla once all targets are hit.
         if (!spinnerSpawned && (AllTargetsHit()||debugStartMatch)) 
@@ -216,5 +245,33 @@ public class PreGameTimer : MonoBehaviour
             return true;
         }
         return false;
+    }
+    IEnumerator TutorialSequence()
+    {
+        while (!sign_catch.activeInHierarchy)
+        {
+            if (monkeysActions.Count >= GameManager.Instance.TotalNumberofPlayers)
+            {
+                sign_catch.SetActive(true);
+                monkeysActions.Clear();
+                tutorialState = TutorialState.Catch;
+            }
+            yield return null;
+        }
+        yield return null;
+        while (!sign_climbVines.activeInHierarchy)
+        {
+            if (monkeysActions.Count >= GameManager.Instance.TotalNumberofPlayers)
+            {
+                sign_climbVines.SetActive(true);
+                monkeysActions.Clear();
+            }
+            yield return null;
+        }
+        yield return null;
+    }
+    public void AddMonkeyAction()
+    {
+        monkeysActions.Add(0);
     }
 }
